73c0ddde99dcfcdc0ac701c1e0ab3987380992b3
Rawan Al-Rajabi <rawanalrajabi2016@gmail.com>
2020-05-27 16:55:08 +0300

    Updated the file to have the practice set instructions

---
+++ first-test.js
@@ -0,0 +1,11 @@
+// welcome to first-test.js file, write the console.log code underneath
+
+
+
+// change the sentence inside the `console.log` to say "Hello" then your name
+
+
+
+// Change the sentence again to say "Goodbye" then your name
+
+
