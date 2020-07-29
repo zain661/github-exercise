4f964633a4d4d8b5185dea631638627e48049114
Zain Assaf <zaynassaf2001@gmail.com>
2020-07-29 03:48:37 +0300

    hello zayn

--- updated-index.js
+++ updated-index.js
@@ -1,5 +1,4 @@
 // write the console.log code with hello in it underneath
-console.log("hello");
+console.log("hello zayn");
 
 // write the console.log code with goodbye in it underneath
-console.log("goodbye zain");
